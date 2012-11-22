9c53d7bc6ecacfeb53561a9855d9f00f50f7cb63
Carlo Andaya <carlo@carloandaya.com>
2012-11-21 18:26:48 -0800

    starting the player list view controller

---
+++ LeagueManager/LMPlayerListViewController.h
@@ -0,0 +1,22 @@
+//
+//  LMPlayerListViewController.h
+//  LeagueManager
+//
+//  Created by Carlo Andaya on 11/21/12.
+//  Copyright (c) 2012 Carlo Andaya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class LMMasterViewController, Team, Player;
+
+@interface LMPlayerListViewController : UITableViewController
+
+@property (retain, nonatomic) Team *team;
+@property (retain, nonatomic) LMMasterViewController *rootViewController;
+
+- (id)initWithRootViewController:(LMMasterViewController *)aController team:(Team *)aTeam;
+- (void)showPlayerView;
+- (NSArray *)sortPlayers;
+
+@end
